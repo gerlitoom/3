1cf4acfd1b0ef696aef333bdb728ea5dadd01601
gerltoom <gerltoom@greeny.cs.tlu.ee>
2016-09-22 11:12:55 +0300

    update

---
+++ login.php
@@ -0,0 +1,68 @@
+<?php
+
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+	
+	$signupUsernameError="";
+	$signupUsername="";
+	
+		if(isset ($_POST["signupUsername"]))
+		
+			if (empty ($_POST["signupUsername"])){
+				$signupUsernameError="See vali on kohustuslik";
+			}
+			else {
+				$signupUsername=$_POST["signupUsername"];
+			}
+	$signupEmailError="";
+	$signupEmail = "";
+	
+	if(isset ($_POST["signupEmail"]))
+		
+		if (empty ($_POST["signupEmail"])){
+			$signupEmailError="See vali on kohustuslik";
+		}
+		else {
+			$signupEmail=$_POST["signupEmail"];
+		}
+		
+	$signupPasswordError="";
+	
+	
+	if(isset ($_POST["signupPassword"]))
+		
+		if (empty ($_POST["signupPassword"])){
+			$signupPasswordError="See vali on kohustuslik";
+		}	
+	else
+		if(strlen($_POST["signupPassword"]) < 8) {
+			$signupPasswordError="Parool pole piisavalt pikk";
+		}
+		
+
+		
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sisselogimise leht</title>
+</head>
+<body>
+
+	<h1>Logi sisse</h1><br>
+	<form method="POST">
+		<input placeholder="Kasutajanimi / Email" name="loginEmail" type="text"><br><br>
+		<input placeholder="Parool" name="loginPassword" type="password"><br><br>
+		<input type="submit" value="Logi sisse">
+	</form>
+	<h1>Loo kasutaja</h1><br>
+	<form method="POST">
+		<input placeholder="Kasjutajanimi" name="signupUsername" type="username" value="<?php echo $signupUsername ?>"><?php echo $signupUsernameError; ?><br><br>
+		<input placeholder="Email" name="signupEmail" type="email" value="<?php echo $signupEmail ?>"><?php echo $signupEmailError; ?><br><br>
+		<input placeholder="Parool" name="signupPassword" type="password"><?php echo $signupPasswordError; ?><br><br>
+		<input type="submit" value="Loo kasutaja">
+	</form>
+
+</body>
+</html>
